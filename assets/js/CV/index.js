88e83329f08e0d04205a292b702020c6cc04afc3
Ammar Yasser AllaiThy <ammaryasserallaithy@gmail.com>
2021-08-12 15:53:13 +0200

    Update CV | GADS, Nanodegree

--- assets/js/CV/index.js
+++ assets/js/CV/index.js
@@ -1,5 +1,38 @@
 window.onload = () => {
 
+    const Type = { Android: 0, Full: 1 }
+    const SELECTED_CLASS = 'selected'
+
+    const TABS = document.querySelectorAll('.tab')
+    const PAGES = document.querySelectorAll('.page')
+    const DOWNLOAD_DIV = document.querySelector('.download-cv-div')
+
+    const toggleSelection = (el, add) => el.classList.toggle(SELECTED_CLASS, add)
+
+    const unselect = id => {
+        toggleSelection(TABS[id], false)
+        toggleSelection(PAGES[id], false)
+    }
+
+    const select = id => {
+        unselect(id == 0 ? 1 : 0)
+
+        toggleSelection(TABS[id], true)
+        toggleSelection(PAGES[id], true)
+
+        DOWNLOAD_DIV.style.display = id == 1 ? 'none' : 'block';
+    }
+
+    const view = type => select(type == Type.Android ? 0 : 1)
+
+
+    TABS[0].addEventListener('click', () => view(Type.Android))
+    TABS[1].addEventListener('click', () => view(Type.Full))
+
+
+    // TABS[1].click()
+
+
     const valOfAttr = (el, name) => (v = el.getAttribute(name)) == null ? '' : v
 
     document.querySelectorAll("div.container.triangle").forEach(div =>
@@ -50,7 +83,9 @@ window.onload = () => {
 
                 <div class="value project">
                     <p>
-                        <span class="name label">${valOfAttr(div, 'title')}</span>
+                        <a class="name label" target="_blank" href="${href = valOfAttr(div, 'href')}">
+                            ${valOfAttr(div, 'title')}
+                        </a>
                         <label class="ic">
                             <a target="_blank" href="${href = valOfAttr(div, 'href')}"><i class="${faIcon(href)}"></i></a>
                         </label>
@@ -58,6 +93,20 @@ window.onload = () => {
 
                     <p class="desc">${valOfAttr(div, 'desc')}</p>
                 </div>
+            </div>
+        `
+    })
+
+    document.querySelectorAll("div.scholarship-holder").forEach(div => {
+        notes = valOfAttr(div, 'notes')
+        if (notes != '') notes = `<p> ${notes} </p>`
+
+        div.innerHTML = `
+            <div class="scholarship">
+                <p class="label"> ${valOfAttr(div, 'title')} </p>
+                <p> ${valOfAttr(div, 'org')} </p>
+                <p class="company"> ${valOfAttr(div, 'date')} </p>
+                ${valOfAttr(div, 'notes')}
             </div>
         `
     })
