59266b5dd0b0f407c97217af7351861e5cefec90
Ammar Yasser Allaithy <ammaryasserallaithy@gmail.com>
2024-09-27 00:01:15 +0300

    update profile: apps stats, skills

--- profile/main.js
+++ profile/main.js
@@ -1,11 +1,18 @@
-qsa('div.number-card').forEach(card =>
+qsa('card').forEach(card =>
   card.parentNode.replaceChild(
     creator({
       tag: 'div',
+      cls: 'number-card',
+      style: {
+        background: `#${card.getAttribute('bg')}`
+      },
+      // attrs: {
+      //   number: card.getAttribute('number').replace('+', '')
+      // },
       childs: [
         creator({
-          tag: 'h1',
-          cls: 'card-title',
+          tag: 'h2',
+          cls: 'default card-title',
           text: card.getAttribute('number')
         }),
         creator({
